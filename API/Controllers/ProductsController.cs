c3c84943142d537e0932370ea6d6aa089e9a9117
kiranlimbu <mrlimbu@gmail.com>
2023-02-02 20:38:29 -0800

    End of section 2

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILogger<ProductsController> _logger;
+        private readonly StoreContext _context; // this is dependency injection
+
+        public ProductsController(ILogger<ProductsController> logger, StoreContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
